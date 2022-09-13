50f43de08aa78a8fb071767c8b8dbd852b273c90
K. Albert <al1working.com>
2022-09-13 06:04:46 +0300

    Add regions

--- FSSP/run.py
+++ FSSP/run.py
@@ -29,62 +29,63 @@ def get_links():
         type_of_property_field: [], 
         link_field: [],
     }
+    
+    for region in regions:
+        for status in statuses:
 
-    for status in statuses:
+            i = 1 #Счетчик пагинации
+            empty = '' #Проверка на пустую страницу пагинации
 
-        i = 1 #Счетчик пагинации
-        empty = '' #Проверка на пустую страницу пагинации
+            while True:
 
-        while True:
+                params = {
+                    'torgi[bidnumber]': '',
+                    'torgi[status]': statuses[status],
+                    'torgi[torgpublishdate][from]': '',
+                    'torgi[torgpublishdate][to]': '',
+                    'torgi[propname]': '',
+                    'torgi[region]': region[0],
+                    'torgi[city]': region[1],
+                    'torgi[propertytype][]': '14',
+                    'torgi[startprice][from]': '',
+                    'torgi[startprice][to]': '',
+                    'torgi[torgexpiredate][from]': '',
+                    'torgi[torgexpiredate][to]': '',
+                    'page': f'{i}',
+                }
+                
+                response = requests.get('https://fssp.gov.ru/torgi/ajax_search/', params=params, cookies=cookies, headers=headers)
+                result = response.content
+                soup = bs(result, 'lxml')
+                empty = soup.find('div', class_ = 'empty')
 
-            params = {
-                'torgi[bidnumber]': '',
-                'torgi[status]': statuses[status],
-                'torgi[torgpublishdate][from]': '',
-                'torgi[torgpublishdate][to]': '',
-                'torgi[propname]': '',
-                'torgi[region]': '4300000000000',
-                'torgi[city]': '4300000100000',
-                'torgi[propertytype][]': '14',
-                'torgi[startprice][from]': '',
-                'torgi[startprice][to]': '',
-                'torgi[torgexpiredate][from]': '',
-                'torgi[torgexpiredate][to]': '',
-                'page': f'{i}',
-            }
-            
-            response = requests.get('https://fssp.gov.ru/torgi/ajax_search/', params=params, cookies=cookies, headers=headers)
-            result = response.content
-            soup = bs(result, 'lxml')
-            empty = soup.find('div', class_ = 'empty')
+                if empty:
+                    break
 
-            if empty:
-                break
+                i = i + 1
 
-            i = i + 1
-
-            #Получаем все строки с лотами
-            regex = re.compile('Лот\s\d+')
-            lots_num = soup.find_all(text=regex)
-            for lot in lots_num:
-                link = site + lot.find_parent()['href']
-                data = get_data(link)
-                result_data[address_field].append(data['address'])
-                result_data[price_field].append(data['price'])
-                result_data[organizer_field].append(data['organizer'])
-                result_data[name_field].append(data['name'])
-                result_data[area_field].append(data['area'])
-                result_data[cadastr_field].append(data['cadastr'])
-                result_data[encumbrance_field].append(data['encumbrance'])
-                result_data[descr_encum_field].append(data['descr_encum'])
-                result_data[owner_field].append(data['owner'])
-                result_data[type_of_law_field].append(data['type_of_law'])
-                result_data[start_field].append(data['start'])
-                result_data[finish_field].append(data['finish'])
-                result_data[status_field].append(data['status'])
-                result_data[subject_field].append(data['subject'])
-                result_data[type_of_property_field].append(data['type_of_property'])
-                result_data[link_field].append(link)
+                #Получаем все строки с лотами
+                regex = re.compile('Лот\s\d+')
+                lots_num = soup.find_all(text=regex)
+                for lot in lots_num:
+                    link = site + lot.find_parent()['href']
+                    data = get_data(link)
+                    result_data[address_field].append(data['address'])
+                    result_data[price_field].append(data['price'])
+                    result_data[organizer_field].append(data['organizer'])
+                    result_data[name_field].append(data['name'])
+                    result_data[area_field].append(data['area'])
+                    result_data[cadastr_field].append(data['cadastr'])
+                    result_data[encumbrance_field].append(data['encumbrance'])
+                    result_data[descr_encum_field].append(data['descr_encum'])
+                    result_data[owner_field].append(data['owner'])
+                    result_data[type_of_law_field].append(data['type_of_law'])
+                    result_data[start_field].append(data['start'])
+                    result_data[finish_field].append(data['finish'])
+                    result_data[status_field].append(data['status'])
+                    result_data[subject_field].append(data['subject'])
+                    result_data[type_of_property_field].append(data['type_of_property'])
+                    result_data[link_field].append(link)
 
     export_to_csv(result_data)
 
